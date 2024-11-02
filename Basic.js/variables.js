7e28a45e12b936f58b14f4807898f53da9e9a3ea
Tarunkanaujia <153187511+Tarunkanaujia@users.noreply.github.com>
2024-11-02 12:51:37 +0000

    config done

---
+++ Basic.js/variables.js
@@ -0,0 +1,26 @@
+//Varibales----> 
+
+/* In JavaScript, a variable is a named container that holds data values.
+It acts as a reference to a value stored in memory, allowing you to store, retrieve, and manipulate that
+ value throughout your program.
+
+ Declaration: You create a variable using the keywords var, let, or const.
+
+ var: Function-scoped or globally scoped, can be re-declared.
+
+let: Block-scoped, cannot be re-declared in the same block, suitable for variables that change. 
+
+const: Block-scoped, cannot be re-declared or reassigned, used for constants. */
+
+
+const id = 1234;
+// id = 12354;  // Not allowed
+console.log(id);
+
+
+
+Accountname = "Noobie";
+Accounttype = "Saving"
+Gamilid = " Noobie@gmail.com"
+
+console.table([Accountname,Accounttype,Gamilid]); // if you want to run multiple variables at a time so we can use "table"
