6da16a19d15f7570b6c0df152f6d6154eb51ec00
Tarunkanaujia <153187511+Tarunkanaujia@users.noreply.github.com>
2025-06-06 07:28:50 +0000

    updated

---
+++ Basic.js/Strings.js
@@ -0,0 +1,16 @@
+let name = "tarun"   // first way to declare string
+let lname = "Kanaujia"
+let age = 25;
+console.log(name + lname);  // here we are concate two strings.  first Way
+console.log(`My name is ${name}${lname} and i am  ${age}yr old`); // using string interpolation in backtick.  Second Way
+
+
+let NewName = new String('Manish sharma')   //Second way to declare string
+console.log(NewName);
+console.log(NewName[0]);
+
+console.log(NewName.toUpperCase());
+console.log(NewName.charAt(5));
+console.log(NewName.indexOf('h'));
+
+
