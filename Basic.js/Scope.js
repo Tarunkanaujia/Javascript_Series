f38efbb1567c8db35e32dd28c3fc7e40ae9b8eff
Tarunkanaujia <153187511+Tarunkanaujia@users.noreply.github.com>
2025-06-11 11:40:21 +0000

    scope level and mini hoisting

--- Basic.js/Scope.js
+++ Basic.js/Scope.js
@@ -4,9 +4,53 @@
   if(true){
     let a = 25
     const b = 250
-    console.log ("Inner",a)
-    console.log ("Inner",b)
+    // console.log ("Inner",a)
+    // console.log ("Inner",b)
   }
 
-    console.log ("outer",a)
-    console.log ("outer",b)
+    // console.log ("outer",a)
+    // console.log ("outer",b)
+
+// ****************Function Scope check ******************
+
+    function one (){
+      const username = "Tarun"
+      function two(){
+        const website = "Youtube"
+        console.log(username);
+        
+      }
+      // console.log(website);
+      two()
+    }
+    // one()
+
+
+    // *************** Through loops ********************
+
+
+    if(true){
+      const username = "Virat"
+      if(username === "Virat"){
+        const website = "Youtube"
+    //  console.log(username + website);
+     
+      }
+      // console.log(website);
+      
+    }
+      // console.log(username);
+
+
+      // *************************************
+
+      one(4)   // Can access 'One' before initialization
+      function one (num){
+        return num + 1 
+      }
+
+      
+      holdfun(5)  // Cannot access 'holdfun' before initialization
+       const holdfun = function one (num){
+        return num + 1 
+      }
