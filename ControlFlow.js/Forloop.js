d4c50d0c5dc9fbf6fc4929439f514092ee34aa08
Tarunkanaujia <153187511+Tarunkanaujia@users.noreply.github.com>
2025-06-14 13:42:54 +0000

    for

---
+++ ControlFlow.js/Forloop.js
@@ -0,0 +1,39 @@
+for (let i = 0 ; i <= 10 ; i++){
+    const value = i
+    if(value == 5){
+        // console.log("this is my lucky number.");
+        
+    }
+    // console.log(value)
+}
+// console.log("check" , value)
+
+// Print table *****************************************
+
+for( let i = 0 ; i <= 10 ; i++){
+//   console.log(i)
+console.log(`${i} ka Table Start.`)
+console.log("")
+
+  for( let j = 0 ; j <= 10 ; j++){
+  console.log(`${i} * ${j} = ` , i*j)
+}
+console.log("")
+}
+
+// Printing array.length using for loop.
+
+const superHero = ["Ironman" , "Batman", "Spiderman" , "Hulk" , "Doga"]
+
+for (let index = 0; index <superHero.length; index++) {
+    const element =superHero[index];
+    console.log(element)
+    
+}
+
+
+
+// Break and Continue
+
+
+
